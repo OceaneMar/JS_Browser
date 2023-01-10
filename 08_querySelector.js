ce969fad45cc65f8cf2d78eafbe2ef28b29b7077
Oceane Marchand <oceane.mar.pro@gmail.com>
2023-01-10 10:08:40 +0100

    fix ex8

--- 08_querySelector.js
+++ 08_querySelector.js
@@ -1,17 +1,20 @@
 //récupérer les boutons
-let btn = document.querySelectorAll( "button, .btn, .danger, .secondary, .warning" );
+let btn = document.querySelectorAll("button");
 
-CSS(btn, {background: blue;});
+btn = [...btn]; //transformer en tableau
 
-// btn = [...btn]; //transformer en tableau
+//boucle à travers le tableau de bouton pour récupérer la classe de chaque bouton
+for (let i = 0; i < btn.length; i++) {
+    let btnClass = btn[i].classList;
+};
 
-// console.log(btn);
-
-
-// btn.map(elt => {
-//     if (elt = elt[3]) {
-//         return elt.style.backgroundColor="red";
-//     } else {
-//         return elt.style.backgroundColor="blue";
-//     }
-// });
+//Appliquer style à chaque bouton selon sa classe + priorité
+if (btnClass.contains("danger")) {
+    btn[i].style.backgroundColor = "red";
+} else if (btnClass.contains("warning")) {
+    btn[i].style.backgroundColor = "yellow";
+} else if (btnClass.contains("secondary")) {
+    btn[i].style.backgroundColor = "grey";
+} else {
+    btn[i].style.backgroundColor = "blue";
+};
