cce582deb316862c51c7c0f7be4f29a91c52b54f
Oceane Marchand <oceane.mar.pro@gmail.com>
2023-01-11 17:13:43 +0100

    fix 20

--- 20_dynamicFont.js
+++ 20_dynamicFont.js
@@ -1,14 +1,14 @@
-let inputEvent = document.getElementsByClassName("ex20");
+let inputEvent = document.getElementsByClassName("ex20")[0];
 
 console.log("inputEvent :", inputEvent);
 
-inputEvent.addEventListener("submit", () => {
-
+inputEvent.addEventListener("submit", (event) => {
+    event.preventDefault()
     // Récupérer la valeur du champ pour la taille de la police
-let sizeFont = document.getElementById("fontsize").value;
+    let sizeFont = document.getElementById("fontsize").value;
 
-//Sélectionner les éléments et leur appliquer la size
-let eltClass = document.querySelectorAll(".controlled_fontsize");
-eltClass = [...eltClass];
-eltClass.map(i => i.style.fontSize = sizeFont + "px");
+    //Sélectionner les éléments et leur appliquer la size
+    let eltClass = document.querySelectorAll(".controlled_fontsize");
+    eltClass = [...eltClass];
+    eltClass.map(i => i.style.fontSize = sizeFont + "px");
 });
