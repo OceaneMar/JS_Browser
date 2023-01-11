d4e11e90b18a03edcf198366bc023407571065a8
Oceane Marchand <oceane.mar.pro@gmail.com>
2023-01-11 11:24:06 +0100

    exo16

--- 16_contactForm.js
+++ 16_contactForm.js
@@ -3,11 +3,11 @@ let nameForm = document.getElementsByName("name")[0].value;
 let ageForm = document.getElementsByName("age")[0].value;
 let birthdateForm = document.getElementsByName("birthdate")[0].value;
 
-//ajouter le texte + valeurs champs dans un <p> (test dans content)
-let content = document.getElementById("content");
+//ajouter le texte + valeurs champs dans un <p>
+let newP = document.body;
 
 let p = document.createElement("p");
 p.id="result";
 p.textContent = `name: ${nameForm}, age: ${ageForm}, birthdate: ${birthdateForm}`;
 
-content.appendChild(p);
+newP.appendChild(p);
