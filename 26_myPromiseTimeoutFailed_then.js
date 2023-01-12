e42bf0d230d88e8fb9487912c38967d11c90b8e3
Oceane Marchand <oceane.mar.pro@gmail.com>
2023-01-12 12:23:53 +0100

    fix ex26

--- 26_myPromiseTimeoutFailed_then.js
+++ 26_myPromiseTimeoutFailed_then.js
@@ -13,34 +13,35 @@ const btnFail = document.querySelector("button#fail");
 let content = document.body;
 
 btnSuccess.addEventListener("click", async () => {
-    let result = "";
-    try {
-        result = await sleepThrow(1998);
-        let p = document.createElement("p");
-        p.classList.add("success");
-        p.textContent = "J'ai réussi";
-        content.appendChild(p);
-    } catch (e) {
-        console.log("une erreur est survenue:", e);
-        let p = document.createElement("p");
-        p.classList.add("error");
-        p.textContent = "J'ai échoué";
-        content.appendChild(p);
-    };
+    sleepThrow(1998)
+        .then(() => {
+            let p = document.createElement("p");
+            p.classList.add("success");
+            p.textContent = "J'ai réussi";
+            content.appendChild(p);
+        })
+        .catch((e) => {
+            console.log("une erreur est survenue:", e);
+            let p = document.createElement("p");
+            p.classList.add("error");
+            p.textContent = "J'ai échoué";
+            content.appendChild(p);
+        })
 });
 
 btnFail.addEventListener("click", async () => {
-    let result = "";
-    try {
-        result = await sleepThrow(2000);
-        let p = document.createElement("p");
-        p.textContent = "J'ai réussi";
-        content.appendChild(p);
-    } catch (e) {
-        console.log("une erreur est survenue:", e);
-        let p = document.createElement("p");
-        p.classList.add("error");
-        p.textContent = "J'ai échoué";
-        content.appendChild(p);
-    };
+    sleepThrow(2000)
+        .then(() => {
+            let p = document.createElement("p");
+            p.classList.add("success");
+            p.textContent = "J'ai réussi";
+            content.appendChild(p);
+        })
+        .catch((e) => {
+            console.log("une erreur est survenue:", e);
+            let p = document.createElement("p");
+            p.classList.add("error");
+            p.textContent = "J'ai échoué";
+            content.appendChild(p);
+        })
 });
